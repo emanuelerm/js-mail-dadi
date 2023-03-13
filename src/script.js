9c64bd72a704470159f714d1b761bd8247991e3a
emanuelerm <emanueler00@gmail.com>
2023-03-13 16:10:13 +0100

    email validator

--- src/script.js
+++ src/script.js
@@ -10,3 +10,17 @@ Prima di partire a scrivere codice poniamoci qualche domanda:
 Che ci sia un array da qualche parte?
 Se dobbiamo confrontare qualcosa che "cosa" ci serve?
 */
+
+// EMAIL   
+
+const email = document.getElementById("userEmail");
+const password = document.getElementById("userPassword");
+
+let emailCorrect = "";
+const emailValidator = "" + "@boolean.it"
+
+if (emailCorrect === emailValidator) {
+    console.log(emailCorrect);
+} else {
+    alert("Your email address isn't correct")
+}
